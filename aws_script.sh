1aa0a4d8416fbba4c49d0d1544b48847a7f6e826
Bing-Jyue Chen <chenbingjyue@gmail.com>
2023-10-02 03:05:58 -0500

    for aws

--- aws_script.sh
+++ aws_script.sh
@@ -2,13 +2,15 @@
 cargo build --release
 mkdir params_ipa
 mkdir params_kzg
-./target/release/time_circuit examples/benchmark/converted_model_20.msgpack examples/benchmark/example_inp_20.msgpack ipa >> experiment.txt
-./target/release/time_circuit examples/benchmark/converted_model_40.msgpack examples/benchmark/example_inp_40.msgpack ipa >> experiment.txt
-./target/release/time_circuit examples/benchmark/converted_model_80.msgpack examples/benchmark/example_inp_80.msgpack ipa >> experiment.txt
-./target/release/time_circuit examples/benchmark/converted_model_160.msgpack examples/benchmark/example_inp_160.msgpack ipa >> experiment.txt
-./target/release/time_circuit examples/benchmark/converted_model_20.msgpack examples/benchmark/example_inp_20.msgpack kzg >> experiment.txt
-./target/release/time_circuit examples/benchmark/converted_model_40.msgpack examples/benchmark/example_inp_40.msgpack kzg >> experiment.txt
-./target/release/time_circuit examples/benchmark/converted_model_80.msgpack examples/benchmark/example_inp_80.msgpack kzg >> experiment.txt
-./target/release/time_circuit examples/benchmark/converted_model_160.msgpack examples/benchmark/example_inp_160.msgpack kzg >> experiment.txt
+./target/release/time_circuit testing/data/cifar10/model_0.msgpack testing/data/cifar10/example_inp.msgpack ipa > debug_cifar10_ipa.txt
+./target/release/time_circuit testing/data/cifar10/model_0.msgpack testing/data/cifar10/example_inp.msgpack kzg > debug_cifar10_kzg.txt
+./target/release/time_circuit testing/data/gpt2/model_0.msgpack testing/data/gp2/example_inp.msgpack ipa > debug_gpt2_ipa.txt
+./target/release/time_circuit testing/data/gpt2/model_0.msgpack testing/data/gp2/example_inp.msgpack kzg > debug_gpt2_kzg.txt
+./target/release/time_circuit testing/data/mobilenet_1.0/model_0.msgpack testing/data/mobilenet_1.0/example_inp.msgpack ipa > debug_mobilenet_1.0_ipa.txt
+./target/release/time_circuit testing/data/mobilenet_1.0/model_0.msgpack testing/data/mobilenet_1.0/example_inp.msgpack kzg > debug_mobilenet_1.0_kzg.txt
+./target/release/time_circuit testing/data/mobilenet_1.4/model_0.msgpack testing/data/mobilenet_1.4/example_inp.msgpack ipa > debug_mobilenet_1.4_ipa.txt
+./target/release/time_circuit testing/data/mobilenet_1.4/model_0.msgpack testing/data/mobilenet_1.4/example_inp.msgpack kzg > debug_mobilenet_1.4_kzg.txt
+./target/release/time_circuit testing/data/twitter2/model_0.msgpack testing/data/twitter2/example_inp.msgpack ipa > debug_twitter2_ipa.txt
+./target/release/time_circuit testing/data/twitter2/model_0.msgpack testing/data/twitter2/example_inp.msgpack kzg > debug_twitter2_kzg.txt
 
 curl -d "AWS: benchmark models successfully complete" ntfy.sh/bjchen4
