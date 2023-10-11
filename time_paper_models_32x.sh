5110ce28382b7756368507d5a3abd008ea019a91
Bing-Jyue Chen <chenbingjyue@gmail.com>
2023-10-10 22:24:50 -0500

    update 32x

--- time_paper_models_32x.sh
+++ time_paper_models_32x.sh
@@ -8,31 +8,31 @@ export MEASURE=1
 # ipa
 
 # mobilenet_1.4 best: estimated_time=2971.040 | row=16641751 | k=24 | c=15 | i=0
-./target/release/time_circuit data/mobilenet_1.4/model_best.msgpack data/mobilenet_1.4/example_inp.msgpack ipa > data/mobilenet_1.4/model_best_ipa.txt
-rm pkey
+#./target/release/time_circuit data/mobilenet_1.4/model_best.msgpack data/mobilenet_1.4/example_inp.msgpack ipa > data/mobilenet_1.4/model_best_ipa.txt
+#rm pkey
 # mobilenet_1.4 40: estimated_time=8760.331 | row=9142025 | k=24 | c=40 | i=0
-./target/release/time_circuit data/mobilenet_1.4/model_40.msgpack data/mobilenet_1.4/example_inp.msgpack ipa > data/mobilenet_1.4/model_40_ipa.txt
-rm pkey
+#./target/release/time_circuit data/mobilenet_1.4/model_40.msgpack data/mobilenet_1.4/example_inp.msgpack ipa > data/mobilenet_1.4/model_40_ipa.txt
+#rm pkey
 
 # gpt best: | k=24 | c=25 | i=0
 ./target/release/time_circuit data/gpt2/model_best.msgpack data/gpt2/example_inp.msgpack ipa > data/gpt2/model_best_ipa.txt
 rm pkey
 # gpt 40: | k=24 | c=40 | i=0
-./target/release/time_circuit data/gpt2/model_best.msgpack data/gpt2/example_inp.msgpack ipa > data/gpt2/model_40_ipa.txt
+./target/release/time_circuit data/gpt2/model_40.msgpack data/gpt2/example_inp.msgpack ipa > data/gpt2/model_40_ipa.txt
 rm pkey
 
 # kzg
 
 # mobilenet_1.4 best: estimated_time=2971.040 | row=16641751 | k=24 | c=15 | i=0
-./target/release/time_circuit data/mobilenet_1.4/model_best.msgpack data/mobilenet_1.4/example_inp.msgpack kzg > data/mobilenet_1.4/model_best_kzg.txt
-rm pkey
+#./target/release/time_circuit data/mobilenet_1.4/model_best.msgpack data/mobilenet_1.4/example_inp.msgpack kzg > data/mobilenet_1.4/model_best_kzg.txt
+#rm pkey
 # mobilenet_1.4 40: estimated_time=8760.331 | row=9142025 | k=24 | c=40 | i=0
-./target/release/time_circuit data/mobilenet_1.4/model_40.msgpack data/mobilenet_1.4/example_inp.msgpack kzg > data/mobilenet_1.4/model_40_kzg.txt
-rm pkey
+#./target/release/time_circuit data/mobilenet_1.4/model_40.msgpack data/mobilenet_1.4/example_inp.msgpack kzg > data/mobilenet_1.4/model_40_kzg.txt
+#rm pkey
 
 # gpt best: | k=24 | c=25 | i=0
-./target/release/time_circuit data/gpt2/model_best.msgpack data/gpt2/example_inp.msgpack ipa > data/gpt2/model_best_kzg.txt
+./target/release/time_circuit data/gpt2/model_best.msgpack data/gpt2/example_inp.msgpack kzg > data/gpt2/model_best_kzg.txt
 rm pkey
 # gpt 40: | k=24 | c=40 | i=0
-./target/release/time_circuit data/gpt2/model_best.msgpack data/gpt2/example_inp.msgpack ipa > data/gpt2/model_40_kzg.txt
+./target/release/time_circuit data/gpt2/model_40.msgpack data/gpt2/example_inp.msgpack kzg > data/gpt2/model_40_kzg.txt
 rm pkey
