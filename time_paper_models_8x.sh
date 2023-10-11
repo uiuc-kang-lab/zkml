531eaf49116cfc05659b6666f69b561fccdba6c9
Bing-Jyue Chen <chenbingjyue@gmail.com>
2023-10-11 15:34:03 -0500

    simple exp

--- time_paper_models_8x.sh
+++ time_paper_models_8x.sh
@@ -7,34 +7,32 @@ export MEASURE=1
 
 # ipa
 
-# cifar10 best: estimated_time=71.474 | row=260851 | k=18 | c=27 | i=0
+# cifar10 best: estimated_time=76.804 | row=261744 | k=18 | c=29 | i=0
 ./target/release/time_circuit data/cifar10/model_best.msgpack data/cifar10/example_inp.msgpack ipa > data/cifar10/model_best_ipa.txt
 rm pkey
-# cifar10 40: estimated_time=110.920 | row=182923 | k=18 | c=40 | i=0
-./target/release/time_circuit data/cifar10/model_40.msgpack data/cifar10/example_inp.msgpack ipa > data/cifar10/model_40_ipa.txt
+# cifar10 40: estimated_time=110.920 | row=189793 | k=18 | c=40 | i=0
+./target/release/time_circuit data/cifar10/model_40.msgpack data/cifar10/example_inp_40.msgpack ipa > data/cifar10/model_40_ipa.txt
 rm pkey
-# cifar10 best diff: estimated_time=1270.840 | row=3620156 | k=22 | c=27 | i=1
-./target/release/time_circuit data/cifar10/model_best_diff.msgpack data/cifar10/example_inp.msgpack ipa > data/cifar10/model_best_diff_ipa.txt
+# cifar10 best diff: estimated_time=1203.919 | row=3354158 | k=22 | c=29 | i=1
+./target/release/time_circuit data/cifar10/model_best_diff.msgpack data/cifar10/example_inp_diff.msgpack ipa > data/cifar10/model_best_diff_ipa.txt
 rm pkey
 
-# dlrm best: estimated_time=38.562 | row=131058 | k=17 | c=27 | i=0
+# dlrm best: estimated_time=47.313 | row=126998 | k=17 | c=33 | i=0
 ./target/release/time_circuit data/dlrm/model_best.msgpack data/dlrm/example_inp.msgpack ipa > data/dlrm/model_best_ipa.txt
 rm pkey
 # dlrm 40: estimated_time=59.293 | row=91053 | k=17 | c=40 | i=0
-./target/release/time_circuit data/dlrm/model_40.msgpack data/dlrm/example_inp.msgpack ipa > data/dlrm/model_40_ipa.txt
+./target/release/time_circuit data/dlrm/model_40.msgpack data/dlrm/example_inp_40.msgpack ipa > data/dlrm/model_40_ipa.txt
 rm pkey
-# dlrm best diff: estimated_time=1193.792 | row=2221303 | k=22 | c=27 | i=1
-./target/release/time_circuit data/dlrm/model_best_diff.msgpack data/dlrm/example_inp.msgpack ipa > data/dlrm/model_best_diff_ipa.txt
+# dlrm best diff: estimated_time=725.999 | row=1751262 | k=21 | c=33 | i=1
+./target/release/time_circuit data/dlrm/model_best_diff.msgpack data/dlrm/example_inp_diff.msgpack ipa > data/dlrm/model_best_diff_ipa.txt
+rm pkey
 
 # vgg16 best: estimated_time=737.912 | row=3731556 | k=22 | c=13 | i=0
-./target/release/time_circuit data/vgg16/model_best.msgpack data/vgg16/example_inp.msgpack ipa > data/vgg16/model_best_ipa.txt
-rm pkey
+#./target/release/time_circuit data/vgg16/model_best.msgpack data/vgg16/example_inp.msgpack ipa > data/vgg16/model_best_ipa.txt
+#rm pkey
 # vgg16 40: estimated_time=1178.605 | row=1930746 | k=21 | c=40 | i=0
-./target/release/time_circuit data/vgg16/model_40.msgpack data/vgg16/example_inp.msgpack ipa > data/vgg16/model_40_ipa.txt
-rm pkey
-# vgg16 best diff: estimated_time=10267.363 | row=57767386 | k=26 | c=13 | i=1
-./target/release/time_circuit data/vgg16/model_best_diff.msgpack data/vgg16/example_inp.msgpack ipa > data/vgg16/model_best_diff_ipa.txt
-rm pkey
+#./target/release/time_circuit data/vgg16/model_40.msgpack data/vgg16/example_inp.msgpack ipa > data/vgg16/model_40_ipa.txt
+#rm pkey
 
 # twitter2 best: estimated_time=477.073 | row=4109452 | k=22 | c=15 | i=0
 #./target/release/time_circuit data/twitter2/model_best.msgpack data/twitter2/example_inp.msgpack ipa > data/twitter2/model_best_ipa.txt
@@ -45,34 +43,35 @@ rm pkey
 
 # kzg
 
-# cifar10 best
+# cifar10 best: estimated_time=76.804 | row=261744 | k=18 | c=29 | i=0
 ./target/release/time_circuit data/cifar10/model_best.msgpack data/cifar10/example_inp.msgpack kzg > data/cifar10/model_best_kzg.txt
 rm pkey
-# cifar10 40
-./target/release/time_circuit data/cifar10/model_40.msgpack data/cifar10/example_inp.msgpack kzg > data/cifar10/model_40_kzg.txt
+# cifar10 40: estimated_time=110.920 | row=189793 | k=18 | c=40 | i=0
+./target/release/time_circuit data/cifar10/model_40.msgpack data/cifar10/example_inp_40.msgpack kzg > data/cifar10/model_40_kzg.txt
 rm pkey
-# cifar10 best diff
-./target/release/time_circuit data/cifar10/model_best_diff.msgpack data/cifar10/example_inp.msgpack kzg > data/cifar10/model_best_diff_kzg.txt
+# cifar10 best diff: estimated_time=1203.919 | row=3354158 | k=22 | c=29 | i=1
+./target/release/time_circuit data/cifar10/model_best_diff.msgpack data/cifar10/example_inp_diff.msgpack kzg > data/cifar10/model_best_diff_kzg.txt
 rm pkey
 
-# dlrm best
+# dlrm best: estimated_time=47.313 | row=126998 | k=17 | c=33 | i=0
 ./target/release/time_circuit data/dlrm/model_best.msgpack data/dlrm/example_inp.msgpack kzg > data/dlrm/model_best_kzg.txt
 rm pkey
-# dlrm 40
-./target/release/time_circuit data/dlrm/model_40.msgpack data/dlrm/example_inp.msgpack kzg > data/dlrm/model_40_kzg.txt
+# dlrm 40: estimated_time=59.293 | row=91053 | k=17 | c=40 | i=0
+./target/release/time_circuit data/dlrm/model_40.msgpack data/dlrm/example_inp_40.msgpack kzg > data/dlrm/model_40_kzg.txt
 rm pkey
-# dlrm best diff
-./target/release/time_circuit data/dlrm/model_best_diff.msgpack data/dlrm/example_inp.msgpack kzg > data/dlrm/model_best_diff_kzg.txt
+# dlrm best diff: estimated_time=725.999 | row=1751262 | k=21 | c=33 | i=1
+./target/release/time_circuit data/dlrm/model_best_diff.msgpack data/dlrm/example_inp_diff.msgpack kzg > data/dlrm/model_best_diff_kzg.txt
+rm pkey
 
 # vgg16 best
-./target/release/time_circuit data/vgg16/model_best.msgpack data/vgg16/example_inp.msgpack kzg > data/vgg16/model_best_kzg.txt
-rm pkey
+#./target/release/time_circuit data/vgg16/model_best.msgpack data/vgg16/example_inp.msgpack kzg > data/vgg16/model_best_kzg.txt
+#rm pkey
 # vgg16 40
-./target/release/time_circuit data/vgg16/model_40.msgpack data/vgg16/example_inp.msgpack kzg > data/vgg16/model_40_kzg.txt
-rm pkey
+#./target/release/time_circuit data/vgg16/model_40.msgpack data/vgg16/example_inp.msgpack kzg > data/vgg16/model_40_kzg.txt
+#rm pkey
 # vgg16 best diff
-./target/release/time_circuit data/vgg16/model_best_diff.msgpack data/vgg16/example_inp.msgpack kzg > data/vgg16/model_best_diff_kzg.txt
-rm pkey
+#./target/release/time_circuit data/vgg16/model_best_diff.msgpack data/vgg16/example_inp.msgpack kzg > data/vgg16/model_best_diff_kzg.txt
+#rm pkey
 
 # mobilenet
 
